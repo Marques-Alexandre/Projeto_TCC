cf1df4ed9fd24e84b8ee6d0d5c8876c5a1656444
Marques-Alexandre <marquesalexandre007@gmail.com>
2022-07-29 22:11:57 -0300

    Criando os obstáculos do jogo - Parte 02

--- Assets/_Game/Scripts/GeradorDeObstaculo.cs
+++ Assets/_Game/Scripts/GeradorDeObstaculo.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GeradorDeObstaculo : MonoBehaviour
 {   
 
-    [SerializeField] private GameObject obstaculo;
+    [SerializeField] private GameObject obstaculo, obstaculoCogumelo;
 
     [SerializeField] private Transform pontoDeGeração;
 
@@ -19,9 +19,18 @@ public class GeradorDeObstaculo : MonoBehaviour
         
         int numeroAleatorio = Random.Range(1, 11);
 
-        if(numeroAleatorio <= 5) {
-           GameObject tempPrefab = Instantiate(obstaculo, pontoDeGeração.position, Quaternion.identity) as GameObject;
+        if(numeroAleatorio <= 6 || numeroAleatorio <= 5) {
+           GameObject tempPrefab = Instantiate(obstaculo,pontoDeGeração.position, Quaternion.identity) as GameObject;
+           //GameObject tempPrefabCogumelo = Instantiate(obstaculoCogumelo, pontoDeGeração.position, Quaternion.identity) as GameObject;
            tempPrefab.transform.parent = pontoDeGeração.transform;
+           //tempPrefabCogumelo.transform.parent = pontoDeGeração.transform;
+
+        } else if (numeroAleatorio > 6 || numeroAleatorio <= 5) {
+            GameObject tempPrefab = Instantiate(obstaculoCogumelo, pontoDeGeração.position, Quaternion.identity) as GameObject;
+            tempPrefab.transform.parent = pontoDeGeração.transform;
         }
     }
+
+
+
 }

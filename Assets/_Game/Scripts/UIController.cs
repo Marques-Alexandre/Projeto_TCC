c84fe05030bf80d66a249f6c56e4ee688c9b7194
Marques-Alexandre <marquesalexandre007@gmail.com>
2022-09-04 20:59:57 -0300

    Criando o botão de Pause do jogo

--- Assets/_Game/Scripts/UIController.cs
+++ Assets/_Game/Scripts/UIController.cs
@@ -9,6 +9,8 @@ public class UIController : MonoBehaviour
     public TMP_Text txtPontuacao;
     private GameController gameController;
 
+    public GameObject panelGame, panelPause;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,4 +24,24 @@ public class UIController : MonoBehaviour
     {
         txtPontuacao.text = gameController.pontuacaoPlayer.ToString("0") + "m";
     }
+
+    public void BotaoPause() {
+        panelGame.gameObject.SetActive(false);
+        panelPause.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+
+    }
+     public void BotaRetornarAoJogo() {
+        panelGame.gameObject.SetActive(true);
+        panelPause.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+     }
+
+     //public void BotaoVoltarMenu() {
+       // panelPause.gameObject.SetActive(false);
+       // panelMenuPrincipal.gameObject.SetActive(true);
+       // panelGameOver.gameObject.SetActive(false);
+       //Time.timeScale = 1f;
+
+    // }
 }

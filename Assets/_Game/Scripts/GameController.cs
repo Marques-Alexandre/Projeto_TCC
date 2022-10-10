187e4a39286c7adc8e9aa5ea46a19963a308b4a8
Marques-Alexandre <marquesalexandre007@gmail.com>
2022-10-10 20:24:03 -0300

    Ajustes finais e finalização do projeto

--- Assets/_Game/Scripts/GameController.cs
+++ Assets/_Game/Scripts/GameController.cs
@@ -54,73 +54,73 @@ public class GameController : MonoBehaviour
     // Função que mostrará todos os desafios do jogo.
     public void MensagemEducativa() {
 
-        if (pontuacaoPlayer == 2000) {
+        if (pontuacaoPlayer == 1500) {
             uIController.panelGame.gameObject.SetActive(false);
             uIController.panelMensagem1.gameObject.SetActive(true);
             Time.timeScale = 0f;
             player.estaMovendo = false;
         }
-        else if (pontuacaoPlayer == 10000) {
+        else if (pontuacaoPlayer == 3000) {
             uIController.panelGame.gameObject.SetActive(false);
             uIController.panelMensagem2.gameObject.SetActive(true);
             Time.timeScale = 0f;
             player.estaMovendo = false;
         }
-        else if (pontuacaoPlayer == 16000) {
+        else if (pontuacaoPlayer == 6000) {
             uIController.panelGame.gameObject.SetActive(false);
             uIController.panelMensagem3.gameObject.SetActive(true);
             Time.timeScale = 0f;
             player.estaMovendo = false;
         }
-        else if (pontuacaoPlayer == 24000) {
+        else if (pontuacaoPlayer == 8500) {
             uIController.panelGame.gameObject.SetActive(false);
             uIController.panelMensagem4.gameObject.SetActive(true);
             Time.timeScale = 0f;
             player.estaMovendo = false;
         }
-         else if (pontuacaoPlayer == 31000) {
+         else if (pontuacaoPlayer == 10000) {
             uIController.panelGame.gameObject.SetActive(false);
             uIController.panelMensagem5.gameObject.SetActive(true);
             Time.timeScale = 0f;
             player.estaMovendo = false;
         }
-         else if (pontuacaoPlayer == 42000) {
+         else if (pontuacaoPlayer == 15000) {
             uIController.panelGame.gameObject.SetActive(false);
             uIController.panelMensagem6.gameObject.SetActive(true);
             Time.timeScale = 0f;
             player.estaMovendo = false;
         }
-         else if (pontuacaoPlayer == 53000) {
+         else if (pontuacaoPlayer == 25000) {
             uIController.panelGame.gameObject.SetActive(false);
             uIController.panelMensagem7.gameObject.SetActive(true);
             Time.timeScale = 0f;
             player.estaMovendo = false;
         }
-         else if (pontuacaoPlayer == 61000) {
+         else if (pontuacaoPlayer == 35000) {
             uIController.panelGame.gameObject.SetActive(false);
             uIController.panelMensagem8.gameObject.SetActive(true);
             Time.timeScale = 0f;
             player.estaMovendo = false;
         }
-         else if (pontuacaoPlayer == 80000) {
+         else if (pontuacaoPlayer == 50000) {
             uIController.panelGame.gameObject.SetActive(false);
             uIController.panelMensagem9.gameObject.SetActive(true);
             Time.timeScale = 0f;
             player.estaMovendo = false;
         }
-         else if (pontuacaoPlayer == 102000) {
+         else if (pontuacaoPlayer == 80000) {
             uIController.panelGame.gameObject.SetActive(false);
             uIController.panelMensagem10.gameObject.SetActive(true);
             Time.timeScale = 0f;
             player.estaMovendo = false;
         }
-         else if (pontuacaoPlayer == 200000) {
+         else if (pontuacaoPlayer == 100000) {
             uIController.panelGame.gameObject.SetActive(false);
             uIController.panelMensagem11.gameObject.SetActive(true);
             Time.timeScale = 0f;
             player.estaMovendo = false;
         }
-            else if (pontuacaoPlayer == 220000) {
+            else if (pontuacaoPlayer == 120000) {
             uIController.panelGame.gameObject.SetActive(false);
             uIController.panelMensagemFinal.gameObject.SetActive(true);
             Time.timeScale = 0f;
